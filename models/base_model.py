4093cdcc148df66d688b81fbbedd7baef0ace2e2
Daniela1026 <cartagenad49@gmail.com>
2022-10-26 20:37:10 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -10,32 +10,33 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """arguments for the constructor of a BaseModel"""
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = value
-                else:
-                    models.storage.new(self)
+                    date_time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, date_time)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
-                    def __str__(self):
-                        """should print instance"""
-                        return ("[{}] ({}) {}".format(self.__class__.__name__,
+            def __str__(self):
+                """should print instance"""
+                return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-                        def save(self):
-                            """updates the public instance attribute updated_at"""
-                            self.updated_at = datetime.now()
-                            models.storage.save()
+                def save(self):
+                    """updates the public instance attribute updated_at"""
+                    self.updated_at = datetime.now()
+                    models.storage.save()
 
-                            def to_dict(self):
-                                """returns a dictionary containing all keys/values"""
-                                my_dict = self.__dict__.copy()
-                                my_dict['created_at'] = self.created_at.isoformat()
-                                my_dict['updated_at'] = self.updated_at.isoformat()
-                                my_dict['__class__'] = self.__class__.__name__
-                                return(my_dict)
+                    def to_dict(self):
+                        """returns a dictionary containing all keys/values"""
+                        my_dict = self.__dict__.copy()
+                        my_dict['created_at'] = self.created_at.isoformat()
+                        my_dict['updated_at'] = self.updated_at.isoformat()
+                        my_dict['__class__'] = self.__class__.__name__
+                        return(my_dict)
