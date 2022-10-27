7abc74933e4fdbf13bce28bf4d95cff8d70542ad
Daniela1026 <cartagenad49@gmail.com>
2022-10-27 11:54:36 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,39 +12,33 @@ class BaseModel:
         """arguments for the constructor of a BaseModel"""
         if kwargs:
             for key, value in kwargs.items():
-                dic = {}
-                dic[key] = value
-                if key == "id":
-                    self.id = value
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "updated_at":
-                    self.updated_at = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime
+                            (value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """ should print: [<class name>] (<self.id>) <self.__dict__> """
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                      self.__dict__))
+        """ String representation """
+        dic = dict(self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, dic)
 
     def save(self):
-        """ updates the public instance attribute
-        updated_at with the current datetime """
+        """ Update and save object """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing
-        all keys/values of __dict__ of the instance """
-        dict = self.__dict__.copy()
-        dict['created_at'] = self.created_at.isoformat()
-        dict['updated_at'] = self.updated_at.isoformat()
-        dict['__class__'] = self.__class__.__name__
-        return(dict)
+        """ Return directory """
+        dic = dict(self.__dict__)
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = datetime.isoformat(dic['created_at'])
+        dic['updated_at'] = datetime.isoformat(dic['updated_at'])
+        return dic
