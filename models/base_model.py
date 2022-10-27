9e4fec270ced3f07e3bfddadd1c4fd7a8f3ffb10
Daniela1026 <cartagenad49@gmail.com>
2022-10-27 10:20:21 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -10,21 +10,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """arguments for the constructor of a BaseModel"""
-        if not kwargs == {}:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-
-                    self.created_at = datetime.strptime(
-                            self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-                    self.updated_at = datetime.strptime(
-                            self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                if key == "created_at":
+                    self.created_at = datetime.strptime(kwargs[key], format)
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs[key], format)
 
                 else:
-                    self.id = str(uuid.uuid4())
+                    self.id = str(uuid4())
                     self.created_at = datetime.now()
-                    self.updated_at = self.created_at
+                    self.updated_at = datetime.now()
                     models.storage.new(self)
+
 
                     def __str__(self):
                         """should print instance"""
