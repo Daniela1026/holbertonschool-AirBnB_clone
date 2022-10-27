af0c811efd345861125cf2ef352e1f8d59c2b552
Daniela1026 <cartagenad49@gmail.com>
2022-10-26 19:19:29 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -14,28 +14,29 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-                if key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.fromisoformat(value))
-        else:
-            self.updated_at = datetime.now()
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            storage.new(self)
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.fromisoformat(value))
 
-                def __str__(self):
-                    """should print instance"""
-                    return ("[{}] ({}) {}".format(self.__class__.__name__,
+                    else:
+                        self.updated_at = datetime.now()
+                        self.id = str(uuid.uuid4())
+                        self.created_at = datetime.now()
+                        storage.new(self)
+
+                        def __str__(self):
+                            """should print instance"""
+                            return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-                def save(self):
-                    """updates the public instance attribute updated_at"""
-                    self.updated_at = datetime.now()
-                    models.storage.save()
+                            def save(self):
+                                """updates the public instance attribute updated_at"""
+                                self.updated_at = datetime.now()
+                                models.storage.save()
 
-                def to_dict(self):
-                    """returns a dictionary containing all keys/values"""
-                    my_dict = self.__dict__.copy()
-                    my_dict['created_at'] = self.created_at.isoformat()
-                    my_dict['updated_at'] = self.updated_at.isoformat()
-                    my_dict['__class__'] = self.__class__.__name__
-                    return(my_dict)
+                                def to_dict(self):
+                                    """returns a dictionary containing all keys/values"""
+                                    my_dict = self.__dict__.copy()
+                                    my_dict['created_at'] = self.created_at.isoformat()
+                                    my_dict['updated_at'] = self.updated_at.isoformat()
+                                    my_dict['__class__'] = self.__class__.__name__
+                                    return(my_dict)
