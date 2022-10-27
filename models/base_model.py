7183c9d8ca270c79fc4aed565caa4f80ccdef20d
Daniela1026 <cartagenad49@gmail.com>
2022-10-27 12:32:03 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,11 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.updated_at = self.created_at = datetime.now()
-            models.storage.new(self)
+                                self.id = str(uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
+                    models.storage.new(self)
+
 
     def __str__(self):
         """prints string representation of base"""
