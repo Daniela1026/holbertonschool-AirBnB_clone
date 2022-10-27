be5e3bc1c39ad9ea88a2622fdeee74eca718f796
Daniela1026 <cartagenad49@gmail.com>
2022-10-26 20:42:11 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,13 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """arguments for the constructor of a BaseModel"""
-         if kwargs:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    date_time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, date_time)
-                elif key != "__class__":
-                    setattr(self, key, value)
+                date_time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, date_time)
+            elif key != "__class__":
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
