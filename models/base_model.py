772e413330fd9c1cb28a890d0a2ee210bcf5a9ef
Daniela1026 <cartagenad49@gmail.com>
2022-10-27 15:11:28 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -23,24 +23,21 @@ class BaseModel:
                     self.id = str(uuid4())
                     self.created_at = datetime.now()
                     self.updated_at = datetime.now()
-                    models.storage.new(self)
-
 
                     def __str__(self):
                         """should print instance"""
-                        bm = self.__class__.__name__
-                        return "[{}] ({}) {}".format(bm, self.id, self.__dict__)
+                        return "[{0}] ({1}) {2}".format(BaseModel.__name__,
+                                         self.id, self.__dict__))
 
                         def save(self):
                             """updates the public instance attribute updated_at"""
                             self.updated_at = datetime.now()
-                            models.storage.save()
 
                             def to_dict(self):
                                 """returns a dictionary containing all keys/values"""
                                 my_dict = self.__dict__.copy()
                                 my_dict['created_at'] = self.created_at.isoformat()
-                                my_dict['updatied_at'] = self.updated_at.isoformat()
-                                my_dict['__class__'] = self.__class__.__name__
+                                my_dict['updated_at'] = self.updated_at.isoformat()
+                                my_dict['__class__'] = BaseModel.__name__
                                 return my_dict
 
