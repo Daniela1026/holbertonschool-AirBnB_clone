0b43f14721e4fc2436c4a56aee28d57219976546
lijobazar <lijobazar@hotmail.com>
2022-10-27 19:20:39 -0700

    correction output

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""Test City"""
+"""
+Test City
+"""
 import unittest
-import pep8
-from models.base_model import BaseModel
 from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
 
 
 class Testcity(unittest.TestCase):
 
-    def test_pep8_conformance_city(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0, "Found style errors")
+    """
+    Test city
+    """
+
+    def test_state_id(self):
+        """
+        Test at id/atribute
+        """
+        city = City
+        self.asserEqual(city.state_id, "")
+
+    def test_name_state(self):
+        """
+        Test at name/atribute
+        """
+        city = City()
+        self.asserEqual(city.name, "")
