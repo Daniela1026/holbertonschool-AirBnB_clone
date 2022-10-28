57d8a6195e9c38e0e3c5c14130a9b78fc785fb4d
lijobazar <lijobazar@hotmail.com>
2022-10-27 19:43:48 -0700

    correction identation

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,26 +1,83 @@
 #!/usr/bin/python3
-"""Test Place"""
+"""
+Test Place
+"""
 import unittest
-import pep8
-from models.base_model import BaseModel
-from models.city import City
 from models.place import Place
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
 
 
-class Testpep8(unittest.TestCase):
-    def test_pep8_conformance_place(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/place.py'])
-        self.assertEqual(result.total_errors, 0, "Found style errors")
+class TestPlace(inittest.TestCase)
+    """ 
+    Test Place
+    """
 
-    def test_class(self):
-        place1 = Place()
-        self.assertEqual(place1.__class__.__name__, "Place")
+    def test_city_id(self):
+        """
+        Test at city/atribute/id
+        """
+        pla = Place()
+        self.assertEqual(pla.city_id, "")
 
-    def test_father(self):
-        place1 = Place()
-        self.assertTrue(issubclass(place1.__class__, BaseModel))
+    def test_user_id(self):
+        """
+        Test at user/atribute/id
+        """
+        pla = Place()
+        self.asserEqual(pla.user_id, "")
+
+    def test_name(self):
+        """
+        Test at name/atribute
+        """
+        pla = Place()
+        self.asserEqueal(pla.number_rooms, 0)
+
+    def test_description(self):
+        """
+        Test at description/atribute
+        """
+        pla = Place()
+        self.asserEqueal(pla.description, "")
+
+    def test_num_rooms(self):
+        """
+        Test at number/rooms
+        """
+        pla = Place()
+        self.asserEqueal(pla.number_rooms, 0)
+
+    def test_num_bathrooms(self):
+        """
+        Test at number bathrooms/atribute
+        """
+        pla = Place()
+        self.asserEqueal(pla.number_bathrooms, 0)
+
+    def test_max_guest(self):
+        """
+        Test at number max the guest/atribute
+        """
+        pla = Place()
+        self.asserEqual(pla.max_guest, 0)
+
+    def test_price_nigth(self):
+        """
+        Test at price to nigth/atribute
+        """
+        pla = Place
+        self.asserEqual(pla.test_price_nigth, 0)
+
+    def test_latitud(self)
+        """
+        Test at latitude/atribute
+        """
+        pla = Place()
+        self.asserEqueal(pla.test_latitud, 0.0)
+
+    def test_amenity_id(self):
+        """
+        Test at amenity id/atribute
+        """
+        pla = Place()
+        self.asserEqual(pla.amenity_id, [])
+
