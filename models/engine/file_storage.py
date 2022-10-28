f28b8a2a12de99c9b89d1fb2addd833d30aad213
Daniela1026 <cartagenad49@gmail.com>
2022-10-28 13:22:49 -0700

    file update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,60 +1,106 @@
 #!/usr/bin/python3
 """
-This module is meant to define the storage class
+Contains the FileStorage class
 """
-from models.base_model import BaseModel
+from os.path import exists
 import json
-import os.path
+from models.base_model import BaseModel
+from models.user import User
+
+
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to
-    instances
-    FileStorage class module
-    """
-    __file_path = "file.json"
+    """File Storage Class"""
+
+    # string - path to the JSON file
+    __file_path = 'file.json'
+    # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects
-        """
-        return FileStorage.__objects
+        """return content of __objects attribute"""
+        return self.__objects
 
     def new(self, obj):
-        """Creates a new objects"""
-        key = obj.__class__.__name__ + '.' + obj.id
-        FileStorage.__objects[key] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        """Saves the objects to JSON"""
-        new_dict = {}
-        for i in self.__objects:
-            new_dict[i] = self.__objects[i].to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            f.write(json.dumps(new_dict, default=str))
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(
+                {
+                    k: (v.to_dict() if not isinstance(v, dict) else v)
+                    for (k, v) in self.__objects.items()
+                }, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised)
-        """
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        ; otherwise, do nothing. If the file doesn’t exist,
+        no exception should be raised)"""
+        if exists(self.__file_path):
+            with open(self.__file_path, "r") as file_path:
+                reloaded_dict = json.load(file_path)
+
+                for obj_key in reloaded_dict.keys():
+                    self.new(BaseModel(**reloaded_dict[obj_key]))
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+
         from models.base_model import BaseModel
-        from models.engine.file_storage import FileStorage
         from models.user import User
-        from models.amenity import Amenity
-        from models.city import City
-        from models.place import Place
         from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
         from models.review import Review
 
-        classes_dict = {'BaseModel': BaseModel, 'Amenity': Amenity,
-                        'City': City, 'Place': Place, 'Review': Review,
-                        'User': User, 'State': State}
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                for i, j in json.load(f).items():
-                    splitted = str(i).split('.')
-                    if splitted[0] in classes_dict.keys():
-                        self.__objects[i] = classes_dict[splitted[0]](**j)
-        except:
-            pass
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
