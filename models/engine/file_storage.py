9ebfd891800ff4fde84804b59d72d72155ee88d7
lijobazar <lijobazar@hotmail.com>
2022-10-28 14:42:42 -0700

    Correction E302 expected 2 blank lines, found 1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,52 +2,46 @@
 """
 Contains the FileStorage class
 """
-from os.path import exists
+from os import pat
 import json
-from models.base_model import BaseModel
-from models.user import User
 
 
 
 class FileStorage:
-    """File Storage Class"""
+    """
+    File Storage Class
+    """
 
-    # string - path to the JSON file
-    __file_path = 'file.json'
-    # dictionary - empty but will store all objects by <class name>.id
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """return content of __objects attribute"""
+        """
+        return content of __objects attribute
+        """
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = f"{obj.__class__.__name__}.{ obj.id}"
+                self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, mode="w", encoding="utf-8") as f:
-            json.dump(
-                {
-                    k: (v.to_dict() if not isinstance(v, dict) else v)
-                    for (k, v) in self.__objects.items()
-                }, f)
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        dict_json = {}
+        for key, value in self.__objects.items():
+            dict_json[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            file.write(json.dumps(dict_json))
 
     def reload(self):
-        """deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists
-        ; otherwise, do nothing. If the file doesn’t exist,
-        no exception should be raised)"""
-        if exists(self.__file_path):
-            with open(self.__file_path, "r") as file_path:
-                reloaded_dict = json.load(file_path)
-
-                for obj_key in reloaded_dict.keys():
-                    self.new(BaseModel(**reloaded_dict[obj_key]))
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references."""
+        """
+        deserializes the JSON file to __objects
+        """
 
         from models.base_model import BaseModel
         from models.user import User
@@ -64,43 +58,8 @@ class FileStorage:
                    "Amenity": Amenity,
                    "Place": Place,
                    "Review": Review}
-        return classes
+        if path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as file:             json_object = json.loads(file.read())
 
-    def attributes(self):
-        """Returns the valid attributes and their types for classname."""
-
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+            for key, value in json_object.items():
+                self.__objects[key] = eval(value['__class__'])(**value)
