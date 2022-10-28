f37d1007e990ebcc8c47b37500ab2b2fc1bfbc29
Daniela Cartagena Restrepo <106104377+Daniela1026@users.noreply.github.com>
2022-10-28 10:13:42 -0500

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,27 +4,28 @@ This module will define all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
-    """ Base class for all common attributes/methods for other classes
- """
+    """ Base class for all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
         """ Method __init__ initialize the all attibutes """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
+      if len(kwargs) > 0:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for k, v in kwargs.items():
+                if k == "__class__":
                     continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+                else:
+                    setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.today()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -38,12 +39,11 @@ class BaseModel:
         """
         Updates all info into the storage
         """
-        self.updated_at = datetime.now()
-        storage.save()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """
-        Dictionary containing all keys/values of __dict__ of the instance  
+        Dictionary containing all keys/values of __dict__ of the instance
         """
         dict_return = self.__dict__.copy()
         dict_return["__class__"] = self.__class__.__name__
