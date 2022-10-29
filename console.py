925429efa440fca3b912b88d01ffeac21deb261f
lijobazar <lijobazar@hotmail.com>
2022-10-28 17:59:47 -0700

    Correction output console

--- console.py
+++ console.py
@@ -1,353 +1,175 @@
 #!/usr/bin/python3
-"""
-Entry point for the AirBNB clone console
-This contains and runs the CMD module and handles th entry
-point for the project
-"""
 
+"""
+The entry point of the command interpreter
+"""
 import cmd
-import shlex
-import models
-import re
-import ast
-import inspect
+import json
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from models import storage
 from models.user import User
+from models.city import City
 from models.amenity import Amenity
-
+from models.review import Review
+from models.place import Place
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
-    listOfProjectClass = ["BaseModel", "City", "Place", "Review", "State",
-                          "User", "Amenity"]
-    intAttrs = ["number_rooms", "number_bathrooms", "max_guest",
-                "price_by_night"]
-    floatAttrs = ["latitude", "longitude"]
+    """
+    An interpreter class inheriting from cmd
+    """
+    model_tags = ["BaseModel", "User", "State",
+                  "City", "Amenity", "Place", "Review"]
+    prompt = "(hbnb) "
 
-    def do_EOF(self, arg):
-        """Exits console"""
+    def do_quit(self, args):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, args):
+        """
+        Quit the console when Ctrl D entered
+        """
         return True
 
     def emptyline(self):
-        """ overwriting the emptyline method """
-        return False
-
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
-
-    def do_create(self, arg):
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    """
-    Creates a new instance of a class and prints ID and saves to file.
-    Usage: create <class name>
-    Usage: <class name>.create()
-    """
-=======
-<<<<<<< HEAD
-    """Creates a new instance of a class and prints ID and saves
-    to file."""
-=======
-    """Creates a new instance of a class and prints ID and saves to file.
-    """
->>>>>>> cd64214365cc8b8f1b7a1b164ed65d31ceb47551
->>>>>>> 8dab1ebf6ac8a2220954e70e5a9547aff88c949f
-=======
         """
-****HELP****
-Creates a new instance of a class and prints ID and saves to file.
-Usage: create <class name>
-Usage: <class name>.create()
-"""
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-        lineAsArgs = shlex.split(arg)
-        if not self.verify_class_in_project(lineAsArgs):
-            return
-        newInstance = eval(str(lineAsArgs[0]) + '()')
-        print(newInstance.id)
-        newInstance.save()
-
-    def do_show(self, arg):
-<<<<<<< HEAD
-        """Prints the string representation of an instance"""
-=======
+        Overrides parent empty line method
         """
-<<<<<<< HEAD
-        Prints the string representation of an instance
-        based on the class name and id.
-        Usage: show <class name> <id>
-        Usage: <class name>.show(<id>)
+        pass
+
+    def do_create(self, args):
         """
-=======
-****HELP****
-Prints the string representation of an instance
-based on the class name and id.
-Usage: show <class name> <id>
-Usage: <class name>.show(<id>)
-"""
-<<<<<<< HEAD
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-=======
->>>>>>> cd64214365cc8b8f1b7a1b164ed65d31ceb47551
->>>>>>> 8dab1ebf6ac8a2220954e70e5a9547aff88c949f
-        lineAsArgs = shlex.split(arg)
-        if not self.verify_class_in_project(lineAsArgs):
-            return
-        if not self.verify_id_exists(lineAsArgs):
-            return
-        objectAsKey = str(lineAsArgs[0]) + '.' + str(lineAsArgs[1])
-        objectsInStorage = models.storage.all()
-        print(objectsInStorage[objectAsKey])
-
-    def do_destroy(self, arg):
-<<<<<<< HEAD
-        """Deletes an instance based on the class name and id"""
-=======
+        Creates a new instance of a specified class and prints
+        instance's unique id
         """
-<<<<<<< HEAD
-        Deletes an instance based on the class name and id
-        Usage: destroy <class name> <id>
-        Usage: <class name>.destroy(<id>)
-        """
-=======
-****HELP****
-Deletes an instance based on the class name and id
-Usage: destroy <class name> <id>
-Usage: <class name>.destroy(<id>)
-"""
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-        lineAsArgs = shlex.split(arg)
-        if not self.verify_class_in_project(lineAsArgs):
-            return
-        if not self.verify_id_exists(lineAsArgs):
-            return
-        objectAsKey = str(lineAsArgs[0]) + '.' + str(lineAsArgs[1])
-        models.storage.all().pop(objectAsKey)
-        models.storage.save()
-
-    def do_all(self, arg):
-<<<<<<< HEAD
-        """Prints list of strings of all instances or specified
-        instances"""
-=======
-        """
-<<<<<<< HEAD
-        Prints list of strings of all instances or specified instances
-        Usage: all - Prints every saved object
-        Usage: all <class name> - prints every saved object of "class name"
-        Usage: <class name>.all()
-        """
-=======
-****HELP****
-Prints list of strings of all instances or specified instances
-Usage: all - Prints every saved object
-Usage: all <class name> - prints every saved object of "class name"
-Usage: <class name>.all()
-"""
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-        lineAsArgs = shlex.split(arg)
-        objectsInStorage = models.storage.all()
-        listOfObjectToPrint = []
-        if len(lineAsArgs) == 0:
-            for value in objectsInStorage.values():
-                listOfObjectToPrint.append(str(value))
-        else:
-            if not self.verify_class_in_project(lineAsArgs):
-                return
-            for (key, value) in objectsInStorage.items():
-                if lineAsArgs[0] in key:
-                    listOfObjectToPrint.append(str(value))
-        print(listOfObjectToPrint)
-
-    def do_update(self, line):
-<<<<<<< HEAD
-        """Updates an instance based on the class name and id."""
-=======
-        """
-<<<<<<< HEAD
-        Updates an instance based on the class name and id.
-        Usage: update <class name> <id>, <attribute name>, <attribute value>
-        Usage: <class name>.update(<id>, <attribute name>, <attribute value>)
-        ****EXTRA****
-        Can take a dictionary as input to update multiple attributes at once
-        Usage: update <class name> <id>, <dictionary representation>
-        usage: <class name>.update(<id>, <dictionary representation>)
-        """
-=======
-****HELP****
-Updates an instance based on the class name and id.
-Usage: update <class name> <id>, <attribute name>, <attribute value>
-Usage: <class name>.update(<id>, <attribute name>, <attribute value>)
-****EXTRA****
-Can take a dictionary as input to update multiple attributes at once
-Usage: update <class name> <id>, <dictionary representation>
-usage: <class name>.update(<id>, <dictionary representation>)"""
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-        lineArgs = shlex.split(line)
-        ArgLineDict = None
-        if not self.verify_class_in_project(lineArgs):
-            return
-        if not self.verify_id_exists(lineArgs):
-            return
-        objAsKey = str(lineArgs[0]) + '.' + str(lineArgs[1])
-        if "{" in line:
-            ArgLineDict = self.check_dictionary_exists(line)
-        if ArgLineDict is None:
-            if not self.verify_attribute_arguments(lineArgs):
-                return
-            self.set_Attribute_correctly(objAsKey, lineArgs[2], lineArgs[3])
-        if isinstance(ArgLineDict, dict):
-            for (key, value) in ArgLineDict.items():
-                self.set_Attribute_correctly(objAsKey, key, value)
-        models.storage.all()[objAsKey].save()
-
-    @staticmethod
-    def check_dictionary_exists(line):
-        """ Method checks if update was passed a dictionary"""
-        lineAsArgs = line.split("{")
-        dictionaryInLine = "{" + lineAsArgs[1]
-        try:
-            typeDictionary = ast.literal_eval(dictionaryInLine)
-        except SyntaxError:
-            return None
-        return (typeDictionary)
-
-    @classmethod
-    def set_Attribute_correctly(cls, objAsKey, key, value):
-        """sets the attributes with the correcy casting"""
-        if key in cls.intAttrs:
-            setattr(models.storage.all()[objAsKey], key, int(value))
-        elif key in cls.floatAttrs:
-            setattr(models.storage.all()[objAsKey], key, float(value))
-        else:
-            setattr(models.storage.all()[objAsKey], key, value)
-
-    def default(self, line):
-        """
-        will ensure input is recycled through to meet contracts
-        outlined in DBC methods
-        """
-        listOfCmdMethods = {"show": self.do_show,
-                            "create": self.do_create,
-                            "update": self.do_update,
-                            "destroy": self.do_destroy,
-                            "all": self.do_all,
-                            "count": self.count_instance}
-        if "." not in line:
-            print("*** unknown syntax: " + line)
-            return
-        if "(" not in line or ")" not in line[-1]:
-            print(line + " *** missing parenthesis")
-            return
-        lineAsArgs = re.findall(r"(.*?)\.(.*?)\((.*?)\)", line)
-        if len(lineAsArgs) == 0:
-            print("*** unknown syntax: " + line)
-            return
-    if lineAsArgs[0][1] not in listOfCmdMethods:
-            print("*** command: " + lineAsArgs[0][1] + " is not reccognised")
-            return
-        className = lineAsArgs[0][0]
-        method = lineAsArgs[0][1]
-        info = lineAsArgs[0][2]
-        argumentString = self.create_argument_string(className, method, info)
-        if argumentString is None:
-            return
-        listOfCmdMethods[method](argumentString)
-
-    @staticmethod
-    def create_argument_string(className, method, info):
-        """ this method is used to create a compatible string argument"""
-        argumentString = className + " "
-        if "," in info:
-            if "{" in info:
-                InfoSplit = re.findall(r'(.*)(\{.*?\})$', info)
-                if len(InfoSplit) == 0:
-                    print("Incorrect dictionary syntax in " + info)
-                    return (None)
-                firstArgument = InfoSplit[0][0].replace(",", "")
-                dictionaryInLine = InfoSplit[0][1]
-                argumentString += firstArgument + dictionaryInLine
-            else:
-                argumentString += info.replace(",", "")
-        else:
-            argumentString += info
-        return (argumentString)
-
-    def help_count(self):
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-        print("\n\")
->>>>>>> cd64214365cc8b8f1b7a1b164ed65d31ceb47551
-=======
-        print("\n\
-****HELP****\n\
-Will count number of instances of a class.\n\
-Usage: <class name>.count()\n\
-")
->>>>>>> 132d0c48c28e62bff744a1712802b2d1a56d5b66
-
-    def count_instance(self, arg):
-        """retrieves number of instances of a class"""
-        count = 0
-        instanceStorage = models.storage.all()
-        arg = shlex.split(arg)
-        for (key, value) in instanceStorage.items():
-            if arg[0] in key:
-                count += 1
-        print(count)
-
-    @classmethod
-    def verify_class_for_default(cls, classNameToCheck):
-        """verify that class being created is defined in the project
-        """
-        if classNameToCheck not in cls.listOfProjectClass:
-            return False
-        return True
-
-    @classmethod
-    def verify_class_in_project(cls, args):
-        """verify that class being created is defined in the project
-        """
-        if len(args) == 0:
+        if not args:
             print("** class name missing **")
-            return False
-        if args[0] not in cls.listOfProjectClass:
+            return
+        if args not in self.model_tags:
             print("** class doesn't exist **")
-            return False
-        return True
+            return
 
-    @staticmethod
-    def verify_id_exists(args):
-        """verify that the ID being called exists"""
-        if len(args) < 2:
-            print("** instance id missing **")
-            return False
-        objects = models.storage.all()
-        string_key = str(args[0]) + '.' + str(args[1])
-        if string_key not in objects.keys():
-            print("** no instance found **")
-            return False
-        return True
+        if args == "BaseModel":
+            base = BaseModel()
+        if args == "User":
+            base = User()
+        if args == "State":
+            base = State()
+        if args == "City":
+            base = City()
+        if args == "Place":
+            base = Place()
+        if args == "Amenity":
+            base = Amenity()
+        if args == "Review":
+            base = Review()
 
-    @staticmethod
-    def verify_attribute_arguments(args):
-        """verify the attribute argument was passed correctly
+        base.save()
+        print(base.id)
+
+    def do_show(self, args):
         """
-        if len(args) < 3:
+        Prints the string repr of an instance based
+        on class name and id
+        """
+        line_arg = args.split()
+        m_name, m_id = line_arg
+
+        if not args:
+            print("** class name missing **")
+            return
+        if m_name not in self.model_tags:
+            print("** class doesn't exist **")
+            return
+        if len(line_arg) == 1:
+            print("** instance id missing **")
+            return
+
+        base_id = storage.all().get(f"{m_name}.{m_id}")
+        if not base_id:
+            print("** no instance found **")
+            return
+
+        print(base_id)
+
+    def do_destroy(self, args):
+        """
+        deletes an instance of a class based on class name and id
+        saves changes to json file
+        """
+        line_arg = args.split()
+        m_name, m_id = line_arg
+
+        if not args:
+            print("** class name missing **")
+            return
+        if m_name not in self.model_tags:
+            print("** class doesn't exist **")
+            return
+        if len(line_arg) == 1:
+            print("** instance id missing **")
+            return
+
+        base_id = storage.all().get(f"{m_name}.{m_id}")
+        if not base_id:
+            print("** no instance found **")
+            return
+
+        del storage.all()[f"{line_arg[0]}.{m_id}"]
+        storage.save()
+
+    def do_all(self, name):
+        """
+        prints, as a list of strings, the string repr of all instances
+        in storage, or all instances of a certain class, if provided
+        """
+        storage_ = storage.all()
+        if name:
+            if name not in self.model_tags:
+                print("** class doesn't exist **")
+                return
+
+        print([str(value) for key, value in storage_.items()])
+
+    def do_update(self, args):
+        """
+        updates or adds an attribute to an instance of a class
+        instance is identified by class name and id
+        only one attribute and value can be updated per call
+        """
+        line_arg = args.split()
+        m_name, m_id, name_attr, value_attr = line_arg
+        if not args:
+            print("** class name missing **")
+            return
+
+        if len(line_arg) == 1:
+            print("** instance id missing **")
+            return
+
+        if len(line_arg) == 2:
             print("** attribute name missing **")
-            return False
-        if len(args) < 4:
+            return
+
+        if len(line_arg) == 3:
             print("** value missing **")
-            return False
-        return True
+            return
+
+        if m_name not in self.model_tags:
+            print("** class doesn't exist **")
+            return
+
+        base = storage.all().get(f"{m_name}.{m_id}")
+        if not base:
+            print("** no instance found **")
+            return
+
+        setattr(storage.all()[f"{line_arg[0]}.{m_id}"],
+                name_attr, value_attr.strip('"'))
+        storage.save()
 
 
 if __name__ == '__main__':
